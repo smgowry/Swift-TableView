0b748eacc6bed30175daf6c7376521be66f631e5
smgowry <sm_gowry@yahoo.com>
2015-08-11 14:01:52 -0700

    Initial Commit

---
+++ Swift TableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift TableView
+//
+//  Created by Gowrie Sammandhamoorthy on 8/11/15.
+//  Copyright (c) 2015 Gowrie Sammandhamoorthy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
